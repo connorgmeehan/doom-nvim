4d78f5546eac2dfaaa279d535e3849097fe8368d
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-03-12 14:12:51 -0500

    binds helper

--- lua/doom/utils/user.lua
+++ lua/doom/utils/user.lua
@@ -1,3 +1,6 @@
+local utils = require("doom.utils")
+local is_plugin_disabled = utils.is_plugin_disabled
+
 local user = {}
 
 -- @param: t
@@ -25,5 +28,34 @@ user.add_or_override_plugin = function(t)
     end
   end
 end
+
+user.insert_binds_into_main_table = function(t)
+  -- enabled?
+  if t[1] then
+    for key, mappings in pairs(t) do
+      if type(key) ~= "number" then
+        table.insert(doom.binds, mappings)
+
+        -- if key = table -> normal ??
+
+        -- visual
+
+        -- select
+
+        -- x
+
+        -- terminal
+
+        -- command
+
+        -- leader
+        -- if not is_plugin_disabled("whichkey") then
+        -- end
+
+      end
+    end
+  end
+end
+
 
 return user
