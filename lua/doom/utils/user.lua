6aaae79666a42a1319c4473dde4a3e8e7177ed3c
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-03-12 16:37:06 -0500

    add recursive build nest tree user util

--- lua/doom/utils/user.lua
+++ lua/doom/utils/user.lua
@@ -29,8 +29,34 @@ user.add_or_override_plugin = function(t)
   end
 end
 
+-- build nest map tree recursive
+local function build_next_level(user_tree)
+  local t_nest = {}
+  for key, user_node in pairs(user_tree) do
+
+    if type(key) == "table" then
+      table.insert(t_nest[3], user_node) -- << LEAF
+
+    elseif type(key) == "string" then
+      local new_branch = {
+        string.format("+%s", key:sub(1,1)),
+        string.format("+%s", key:sub(3)),
+        build_next_level(user_node)
+      }
+      -- attach the branch to the nest tree
+      table.insert(t_nest[3], new_branch) -- insert branch
+    end
+  end
+  return t_nest
+end
+
 user.insert_binds_into_main_table = function(t)
-  -- enabled?
+
+  -- bool enable/dissable
+  -- mode keyword add to all binds if it doesn't exist
+  -- mappings table or key.
+  --
+
   if t[1] then
     for key, mappings in pairs(t) do
       if type(key) ~= "number" then
@@ -50,6 +76,22 @@ user.insert_binds_into_main_table = function(t)
 
         -- leader
         -- if not is_plugin_disabled("whichkey") then
+
+
+        -- -- insert table into
+        -- {
+        --   "<leader>",
+        --   name = "+prefix",
+        -- }
+        --
+        --
+        -- ["g go"] = { .... }
+        --
+        -- if type = table then it is a leaf binding
+        -- if type = string > then it is a tree bind
+
+
+
         -- end
 
       end
