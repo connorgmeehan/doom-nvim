d186043317d0d9b0b68ded13fb3115ac96fc0d64
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-03-14 10:45:00 -0400

    move quickfix cdo into leader tree

--- config.lua
+++ config.lua
@@ -1148,6 +1148,56 @@ if not is_plugin_disabled("whichkey") then
                   -- },
                   -- -- vimgrep search word solo
                   -- -- vimgrep search WORD solo
+                  -- -- :cdo empty
+                  -- {
+                  --    "n",
+                  --    "<leader>rce",
+                  --    ":cdo s//gc<Left><Left><Left>",
+                  --    { silent = false },
+                  --    "Quickfix cdo Empty",
+                  --    "quickfix_cdo_empty",
+                  --    "Quickfix cdo Empty"
+                  -- },
+                  -- -- :cdo replace word
+                  -- {
+                  --    "n",
+                  --    "<leader>rcw",
+                  --    "\"zyiw:cdo s/<c-r>z//gc<Left><Left><Left>",
+                  --    { silent = false },
+                  --    "Quickfix cdo (w)ord",
+                  --    "quickfix_cdo_word_small",
+                  --    "Quickfix cdo (w)ord",
+                  -- },
+                  -- -- :cdo replace WORD
+                  -- {
+                  --    "n",
+                  --    "<leader>rcW",
+                  --    "\"zyiW:cdo s/<c-r>z//gc<Left><Left><Left>",
+                  --    { silent = false },
+                  --    "Quickfix cdo (W)ord",
+                  --    "quickfix_cdo_word_big",
+                  --    "Quickfix cdo (W)ord",
+                  -- },
+                  -- -- :cdo solo word
+                  -- {
+                  --    "n",
+                  --    "<leader>rcs",
+                  --    "\"zyiw:cdo s/\\<<c-r>z\\>//gc<Left><Left><Left>",
+                  --    { silent = false },
+                  --    "Quickfix cdo solo (w)ord",
+                  --    "quickfix_cdo_solo_word_small",
+                  --    "Quickfix cdo solo(w)ord",
+                  -- },
+                  -- -- :cdo solo WORD
+                  -- {
+                  --    "n",
+                  --    "<leader>rcS",
+                  --    "\"zyiW:cdo s/\\<<c-r>z\\>//gc<Left><Left><Left>",
+                  --    { silent = false },
+                  --    "Quickfix cdo solo (W)ord",
+                  --    "quickfix_cdo_solo_word_big",
+                  --    "Quickfix cdo solo (W)ord",
+                  -- },
               }
             }
           },
