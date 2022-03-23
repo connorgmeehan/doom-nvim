a6c06fc16eb4208d3bbb5ab489ff0db198a93c0f
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-03-23 08:18:50 -0400

    migrate more configs

--- config.lua
+++ config.lua
@@ -807,7 +807,313 @@ end
 doom.moll.funcs = funcs
 
 
+----------------------------
+---       AUTOCMDS       ---
+----------------------------
 
+  -- doom.autocmds[augroup_name] = {
+  --   { "BufReadPre", "*.lua", "setlocal sw=2", --[[once and nested are boolean keys here]] },
+  --   { "InsertEnter", "*", function() print("Lua functions are valid!") end, once = true }
+  -- }
+
+--    tmux_reload = {
+--     { 'BufWritePost', '~/.config/tmux/tmux.conf', '! tmux source-file ~/.config/tmux/tmux.conf'}
+-- },
+
+-------------------------
+---       BINDS       ---
+-------------------------
+
+-- i shall use functions in as many places as possible.
+-- rewrite my bindings when I can.
+
+-- create a binds capture ui that allows you to create new bindings on the fly
+-- with a gtd similar workflow.
+-- A ui that allows you to step your way down the binds tree, and if you happen
+-- to create a new leaf node, then we display the branch and then
+-- allow user select what mode, action, name and etc. and then this is inserted
+-- into the binds.lua file, in the root folder. this could be quite fucking cool.
+
+-- local binds = {}
+
+local opts = {
+	sf  = { silent = false },
+	n  = {  noremap = true },
+}
+
+-- set / get visual selection
+local vs = "\"zy"
+local viw = "\"zyiw"
+local viW = "\"zyiW"
+local vg = "<c-r>z"
+
+-----------------------------
+---       READ FILE       ---
+-----------------------------
+
+-- -- navigation + rm arrows
+-- {'i', '<Down>', '<Nop>' },
+-- {'i', '<Left>', '<Nop>' },
+-- {'i', '<Right>', '<Nop>' },
+-- {'i', '<Up>', '<Nop>' },
+-- {'n', '<Down>', '<Nop>' },
+-- {'n', '<Left>', '<Nop>' },
+-- {'n', '<Right>', '<Nop>' },
+-- {'n', '<Up>', '<Nop>' },
+-- {'v', '<Down>', '<Nop>' },
+-- {'v', '<Left>', '<Nop>' },
+-- {'v', '<Right>', '<Nop>' },
+-- {'v', '<Up>', '<Nop>' },
+
+-- -- ctrl move
+-- {'i','<c-l>','<space>'},
+-- {'c','<c-l>','<space>'},
+
+------------------------------
+---       formatting       ---
+------------------------------
+
+-- { "n", "<leader>ai", "mzgg=G`z<cr>", opts.s, "Format", "indent_file", "Indent File" },
+-- { "n", "<leader>au", "viwUe", opts.s, "Format", "upper_case", "Word Upper" },
+-- { "n", "<leader>aU", "viwue", opts.s, "Format", "lower_case", "Word Lower" },
+
+----------------------------------
+---       todo quicklist       ---
+----------------------------------
+
+-- { 'n', '<leader>Tq', '<cmd>TodoQuickFix<cr>',  {}, "Quick Fix", "todo_quick_fix", "Todo Quick Fix" },
+-- { 'n', '<leader>Tt', '<cmd>TodoTrouble<cr>',   {}, "Trouble",   "todo_trouble", "Todo Trouble" },
+-- { 'n', '<leader>Te', '<cmd>TodoTelescope<cr>', {}, "Telescope", "todo_telescope", "Todo Telescope" },
+-- { 'n', '<leader>Tl', '<cmd>TodoLocList<cr>',   {}, "LocList",   "todo_loc_list", "Todo LocList" },
+
+--------------------------------------------
+---       SEARCH & REPLACE               ---
+--------------------------------------------
+
+-- get v sel
+--  https://stackoverflow.com/questions/1533565/how-to-get-visually-selected-text-in-vimscript
+-- THESE TWO HAVE THE BEST SUGGESTIONS
+-- https://jdhao.github.io/2020/03/14/nvim_search_replace_multiple_file/
+-- https://phelipetls.github.io/posts/extending-vim-with-ripgrep/
+
+-- https://www.freecodecamp.org/news/how-to-search-and-replace-across-multiple-files-in-vim/
+-- https://stackoverflow.com/questions/37424689/search-and-replace-in-multiple-files-using-vim
+-- https://irian.to/blogs/how-to-search-and-replace-string-across-multiple-files-in-vim/
+-- https://vim.fandom.com/wiki/Search_and_replace_in_multiple_buffers
+-- https://www.reddit.com/r/vim/comments/n8x2xr/til_find_and_replace_multiple_files_with_and/
+-- https://github.com/brooth/far.vim
+
+-------------------------------------------------
+---       SEARCH POPULATE QUICKFIX LIST       ---
+-------------------------------------------------
+
+
+--------------------------------------------------
+---       REPLACE WORDS IN QUICKFIX LIST       ---
+--------------------------------------------------
+
+-- -- :cdo empty
+-- {
+--    "n",
+--    "<leader>rce",
+--    ":cdo s//gc<Left><Left><Left>",
+--    { silent = false },
+--    "Quickfix cdo Empty",
+--    "quickfix_cdo_empty",
+--    "Quickfix cdo Empty"
+-- },
+-- -- :cdo replace word
+-- {
+--    "n",
+--    "<leader>rcw",
+--    "\"zyiw:cdo s/<c-r>z//gc<Left><Left><Left>",
+--    { silent = false },
+--    "Quickfix cdo (w)ord",
+--    "quickfix_cdo_word_small",
+--    "Quickfix cdo (w)ord",
+-- },
+-- -- :cdo replace WORD
+-- {
+--    "n",
+--    "<leader>rcW",
+--    "\"zyiW:cdo s/<c-r>z//gc<Left><Left><Left>",
+--    { silent = false },
+--    "Quickfix cdo (W)ord",
+--    "quickfix_cdo_word_big",
+--    "Quickfix cdo (W)ord",
+-- },
+-- -- :cdo solo word
+-- {
+--    "n",
+--    "<leader>rcs",
+--    "\"zyiw:cdo s/\\<<c-r>z\\>//gc<Left><Left><Left>",
+--    { silent = false },
+--    "Quickfix cdo solo (w)ord",
+--    "quickfix_cdo_solo_word_small",
+--    "Quickfix cdo solo(w)ord",
+-- },
+-- -- :cdo solo WORD
+-- {
+--    "n",
+--    "<leader>rcS",
+--    "\"zyiW:cdo s/\\<<c-r>z\\>//gc<Left><Left><Left>",
+--    { silent = false },
+--    "Quickfix cdo solo (W)ord",
+--    "quickfix_cdo_solo_word_big",
+--    "Quickfix cdo solo (W)ord",
+-- },
+
+-----------------------
+---       GIT       ---
+-----------------------
+
+-- TODO:
+--
+--  git reset
+--	unstage all
+--
+--	split hunk ???????????????????????
+--
+--	reset hunk
+--	reset buffer
+--	reset hunk
+--
+--	partial hunks // https://github.com/lewis6991/gitsigns.nvim/pull/411
+
+-- WHICH-KEY TREE
+-- ["l"] = {
+--   name = "+git-test",
+--   ["zo"] = { "open lazygit" },
+-- },
+
+-- -- 1. unstage all
+-- { 'n', '<leader>lgr', '<cmd>!git reset<cr>', opts.s },
+-- -- 2. unstage all and stagu hunk under cursor
+-- -- 3. triple -> unstage all, select at cursor, and commit
+-- -- 4. unstage all and commit only current buffer.
+
+-- -- NEOGIT
+-- -- :Neogit " uses tab
+-- -- :Neogit kind=<kind> " override kind
+-- -- :Neogit cwd=<cwd> " override cwd
+-- -- :Neogit commit" open commit popup
+-- -- { 'n', '<leader>lnh', '<cmd>lua require("neogit").open({ kind = "vsplit", cwd = "~" })<CR>', opts.s},
+-- { 'n', '<leader>lj', '<cmd>Neogit<CR>', opts.s},
+-- { 'n', '<leader>lnc', '<cmd>Neogit commit<CR>', opts.s},
+-- { 'n', '<leader>lnl', '<cmd>Neogit pull<CR>', opts.s}, -- TODO: pull
+-- { 'n', '<leader>lnp', '<cmd>Neogit push<CR>', opts.s}, -- TODO: push
+-- { 'n', '<leader>lnb', '<cmd>Neogit branch<CR>', opts.s}, -- TODO: push
+-- { 'n', '<leader>lnv', '<cmd>Neogit log<CR>', opts.s}, -- TODO: push
+-- { 'n', '<leader>lns', '<cmd>Neogit stash<CR>', opts.s}, -- TODO: push
+
+-- -- -- GITSIGNS
+-- { 'n', '<leader>lsn', '<cmd>lua require("gitsigns").next_hunk()<CR>', opts.s},
+-- { 'n', '<leader>lsp', '<cmd>lua require("gitsigns").prev_hunk()<CR>', opts.s},
+-- { 'n', '<leader>lss', '<cmd>lua require("gitsigns").stage_hunk()<CR>', opts.s},
+-- { 'n', '<leader>lsu', '<cmd>lua require("gitsigns").undo_stage_hunk()<CR>', opts.s},
+-- { 'n', '<leader>lsb', '<cmd>lua require("gitsigns").stage_buffer()<CR>', opts.s},
+-- -- TODO: diffthiss > diffview hunk at cursor
+-- -- -- TODO: undo buffer
+-- -- toggle_signs()
+-- -- 	toggle_numhl()
+-- -- 	toggle_linehl()
+-- -- 	toggle_word_diff()
+-- -- 	toggle_current_line_blame()
+-- -- 	refresh()
+
+-- -- -- DIFFVIEW
+-- { 'n', '<leader>ldh', '<cmd>DiffviewOpen<CR>', opts.s},
+-- { 'n', '<leader>ldc', '<cmd>DiffviewClose<CR>', opts.s},
+-- { 'n', '<leader>ldH', '<cmd>DiffviewOpen HEAD~2<CR>', opts.s},
+-- -- { 'n', '<leader>ldH', '<cmd>DiffviewOpen HEAD~2<CR>', opts.s}, -- TODO: prev commit
+-- -- { 'n', '<leader>ldH', '<cmd>DiffviewOpen HEAD~2<CR>', opts.s}, -- TODO: HEAD prev HEAD
+
+-- -- -- VGIT
+-- { 'n', '<leader>lvb', '<cmd>lua require("vgit").buffer_hunk_preview()<CR>', opts.s},
+-- { 'n', '<leader>lV', '<cmd>vgit<CR>', opts.s},
+
+-- -- -- LAZYGIT
+-- -- mappings.map("n", "<leader>go", "<cmd>LazyGit<CR>", opts, "Git", "lazygit", "Open LazyGit")
+-- { 'n', '<leader>lzo', '<cmd>LazyGit<CR>', opts.s},
+
+------------------------------
+---       lightspeed       ---
+------------------------------
+-- https://github.com/ggandor/lightspeed.nvim/discussions/83
+-- `<Plug>Lightspeed_s`  2-character  forward   /-like
+-- `<Plug>Lightspeed_S`  2-character  backward  ?-like
+-- `<Plug>Lightspeed_x`  2-character  forward   X-mode
+-- `<Plug>Lightspeed_X`  2-character  backward  X-mode
+
+-- `<Plug>Lightspeed_f`  1-character  forward   f-like
+-- `<Plug>Lightspeed_F`  1-character  backward  F-like
+-- `<Plug>Lightspeed_t`  1-character  forward   t-like
+-- `<Plug>Lightspeed_T`  1-character  backward  T-like
+
+
+
+----------------------------
+---       surround       ---
+----------------------------
+
+-- -- map ds       <Plug>Dsurround
+-- -- nmap cs       <Plug>Csurround
+-- -- nmap cS       <Plug>CSurround
+-- -- nmap ys       <Plug>Ysurround
+-- -- nmap yS       <Plug>YSurround
+-- -- nmap yss      <Plug>Yssurround
+-- -- nmap ySs      <Plug>YSsurround
+-- -- nmap ySS      <Plug>YSsurround
+-- -- xmap gs       <Plug>VSurround
+-- -- xmap gS       <Plug>VgSurround
+-- normal mode
+
+-- local bind = function(t)
+--   print(t[1], t[2])
+--   -- enabled?
+--   if t[1] then
+--     for key, mappings in pairs(t) do
+--       print("type: ", type(key), key)
+--       if type(key) ~= "number" then
+--         print(key, "<<<<")
+--         table.insert(doom.binds, mappings)
+--       end
+--     end
+--   end
+-- end
+
+local bind = insert_binds_into_main_table -- require("doom.utils.user").insert_binds_into_main_table
+local enabled = true
+local disabled = false
+
+-- -- search regex
+-- {'n', '/', '/\\v'}, -- need to esc backsl
+-- {'v', '/', '/\\v'},
+
+-- bind { -- normal
+--   enabled,
+--   normal = {
+--   { "<c-z>", [[<cmd>suspend<CR>]], name = "suspend vim" },
+--   { "<c-z>", [[<Esc><cmd>suspend<CR>]], name = "suspend vim", mode = "v" },
+--   { ';', ':', name = "colon", options = opts.sf },
+--   { ':', ';', name = "semi-colon" },
+--     --  {'n', 'dl', ':set wrap! linebreak!<CR>'},
+--     -- { 'x', 'z', '<Plug>VSurround' },
+--     -- { 'n', 'yzz', '<Plug>Yssurround' }, -- double ss
+--     -- { 'n', 'yz', '<Plug>Ysurround' }, -- single s
+--     -- { 'n', 'dz', '<Plug>Dsurround' },
+--     -- { 'n', 'cz', '<Plug>Csurround' },
+--   }
+-- }
+
+-- - local binds_operator = {
+-- { 'o', 'b', 'vb', opts.ns },
+-- { 'o', 'B', 'vB', opts.ns },
+-- { 'o', 'F', 'vF', opts.ns },
+-- { 'o', 'T', 'vT', opts.ns },
+-- -- { 'o', 's', '<Plug>Lightspeed_s', opts.n },
+-- -- { 'o', 'S', '<Plug>Lightspeed_S', opts.ns },
+-- }
 
 
 
