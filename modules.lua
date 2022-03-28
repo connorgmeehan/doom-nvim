5edbec9d7de5ffc2a56d4895694a636c413091b2
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-03-28 07:37:01 -0400

    feat(module): github module

--- modules.lua
+++ modules.lua
@@ -8,52 +8,52 @@
 return {
   features = {
     -- Language features (33ms)
-    "annotations",        -- Code annotation generator
-    "auto_install",       -- Auto install LSP providers
-    "autopairs",          -- Automatically close character pairs
-    "comment",            -- Adds keybinds to comment in any language
-    "linter",             -- Linting and formatting for languages
-    "lsp",                -- Code completion
-    "snippets",           -- Code snippets for all languages
+    "annotations", -- Code annotation generator
+    "auto_install", -- Auto install LSP providers
+    "autopairs", -- Automatically close character pairs
+    "comment", -- Adds keybinds to comment in any language
+    "linter", -- Linting and formatting for languages
+    "lsp", -- Code completion
+    "snippets", -- Code snippets for all languages
 
     -- Editor (34ms, 1ms)
-    "auto_session",       -- Remember sessions between loads
-    "colorizer",          -- Show colors in neovim
-    "editorconfig",       -- Support editorconfig files
-    "gitsigns",           -- Show git changes in sidebar
-    "illuminate",         -- Highlight other copies of the word you're hovering on
-    "indentlines",        -- Show indent lines with special characters
-    "range_highlight",    -- Highlight selected range from commands
-    "todo_comments",      -- Highlight TODO: comments
+    "auto_session", -- Remember sessions between loads
+    "colorizer", -- Show colors in neovim
+    "editorconfig", -- Support editorconfig files
+    "gitsigns", -- Show git changes in sidebar
+    "illuminate", -- Highlight other copies of the word you're hovering on
+    "indentlines", -- Show indent lines with special characters
+    "range_highlight", -- Highlight selected range from commands
+    "todo_comments", -- Highlight TODO: comments
 
     -- UI (44ms, 10ms)
-    "lsp_progress",             -- Check status of LSP loading
-    "tabline",            -- Tab bar buffer switcher
-    "dashboard",          -- A pretty dashboard upon opening
-    "trouble",            -- A pretty diagnostic viewer
-    "statusline2",         -- A pretty status line at the bottom of the buffer
-    "minimap",         -- Shows current position in document
+    "lsp_progress", -- Check status of LSP loading
+    "tabline", -- Tab bar buffer switcher
+    "dashboard", -- A pretty dashboard upon opening
+    "trouble", -- A pretty diagnostic viewer
+    "statusline2", -- A pretty status line at the bottom of the buffer
+    "minimap", -- Shows current position in document
 
     -- Tools (188ms, 144ms)
-    "dap",                -- Debug code through neovim
-    "explorer",           -- An enhanced filetree explorer
-    "firenvim",           -- Embed neovim in your browser
-    "telescope",          -- Fuzzy searcher to find files, grep code and more
-    "neorg",              -- Organise your life
-    "whichkey",           -- An interactive sheet
-    "projects",           -- Quickly switch between projects
+    "dap", -- Debug code through neovim
+    "explorer", -- An enhanced filetree explorer
+    "firenvim", -- Embed neovim in your browser
+    "telescope", -- Fuzzy searcher to find files, grep code and more
+    "neorg", -- Organise your life
+    "whichkey", -- An interactive sheet
+    "projects", -- Quickly switch between projects
 
-    "doom_themes",     -- Extra themes for doom
-    "lazygit",         -- Lazy git integration
-    "neogit",          -- A git client for neovim
-    "ranger",          -- File explorer in neovim (TODO: Test)
-    "restclient",      -- Test HTTP requests from neovim (TODO: Test)
-    "show_registers",  -- Show and navigate between registers
+    "doom_themes", -- Extra themes for doom
+    "lazygit", -- Lazy git integration
+    "neogit", -- A git client for neovim
+    "ranger", -- File explorer in neovim (TODO: Test)
+    "restclient", -- Test HTTP requests from neovim (TODO: Test)
+    "show_registers", -- Show and navigate between registers
     -- "suda",            -- Save using sudo when necessary
-    "superman",        -- Read unix man pages in neovim
-    "symbols",         -- Navigate between code symbols using telescope
-    "terminal",        -- Integrated terminal in neovim
-    "zen",             -- Distractionless coding
+    "superman", -- Read unix man pages in neovim
+    "symbols", -- Navigate between code symbols using telescope
+    "terminal", -- Integrated terminal in neovim
+    "zen", -- Distractionless coding
   },
   langs = {
     -- Scripts
@@ -77,12 +77,13 @@ return {
     "kotlin",
     "java",
 
-    "config",             -- JSON, YAML, TOML
+    "config", -- JSON, YAML, TOML
     "markdown",
   },
   user = {
-    'char_counter',
-  }
+    "char_counter",
+    "github",
+  },
 }
 
 -- vim: sw=2 sts=2 ts=2 fdm=indent expandtab
