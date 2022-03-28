72df38debbdd82a1080f0b85f954f53ce362e870
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-03-28 02:22:57 -0400

    neogit test rndm

--- RNDM.lua
+++ RNDM.lua
@@ -1,18 +1,13 @@
 --[[
 
-I NEED TO PULL THE LATEST CHANGES per connors wishes and see what happens with the latest branch
+- 1. need to turn off gpg
+- 2.
 
-arst
+what possibilities are there.
 
-thc its in the game. that is a funny thing to say actually since it is just so fucking fast thaty ou cannot almost even see
+- disable fully
+- cronjo
+- solve bug
 
-you cannot trust them. they are so fucking depressing and so that is the shit that you cannot really fucking do you know.
-i don't fucking understand why the fuck that shit is you know. there are like three people out there that really fucking hate you guys.
-nah sit down. i donfmt understand if that is really the thing that you might never see.
-
-  oh thanks man i think that is quite fucking strange and so that is something that i don't think is possible you know. so why the fuck
-  would they ever do that you know. if they brought it out they would never fucking do that shit which is one of the most insane
-  things that can ever happen. so why the fuck would that happen it is something that you cannot realy do. so why the fuck did you
-  do it.
 
 --]]
