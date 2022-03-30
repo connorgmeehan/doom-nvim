a1d5d76c3d7ffce885569b6a19faccc6e4c61130
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-03-30 08:12:44 -0400

    more modules

--- lua/user/modules/markdown/init.lua
+++ lua/user/modules/markdown/init.lua
@@ -1,6 +1,8 @@
 local md = {}
 
+-- https://github.com/ellisonleao/glow.nvim
 -- https://github.com/iamcco/markdown-preview.nvim
 -- https://github.com/davidgranstrom/nvim-markdown-preview
+-- https://github.com/mjlbach/babelfish.nvim -- md 2 vimdoc
 
 return md
