88f0dd751830da38b84bb0d2432be5a322cd2368
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-01 09:13:37 -0400

    user modules

--- lua/user/modules/games/init.lua
+++ lua/user/modules/games/init.lua
@@ -1,2 +1,10 @@
 local games = {}
+
+games.settings = {}
+
+games.packages = {
+  ["vim-be-good"] = { "ThePrimeagen/vim-be-good" },
+  ["train.nvim"] = { "tjdevries/train.nvim" },
+}
+
 return games
