35c2ed2455b4aa9013f68db1a730511d153ad89e
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-03 04:58:25 -0400

    user modules

--- lua/user/modules/create_module/init.lua
+++ lua/user/modules/create_module/init.lua
@@ -2,9 +2,17 @@ local create_module = {}
 
 create_module.cmd = {
   "CreateModule", function()
-    -- nui input name of module
-    -- create new module with init.lua file based on the input name.
-    -- if exists assert err
+    -- 1. get list / table of existing module names.
+    -- 2. get user input
+    -- 3. does input exist?
+    -- 4. create dir
+    -- 5. create init.lua
+    -- 6. open file into buffer
+    -- 7. insert template for module.
+    -- 8. open the module file in a split window
+    -- 9. use telescope to filter input to see if it exists.
+    -- 10. if select already existing module > open that module file in split
+    -- 11. if it does not exist > create new module.
   end
 }
 
--- lua/user/modules/create_plugin/init.lua
+++ lua/user/modules/create_plugin/init.lua
@@ -1,16 +1,18 @@
 local create_plugin = {}
 
--- if a module is mature enough then move the module into its own plugin.
--- use:
---  boilit
---  refactor
---  ghq
---
---  to migrate a module into a new plugin.
-
 create_plugin.cmd = {
   { "CreatePlugin", function() end },
-  { "CreatePluginMigrateByname", function() end },
+  { "CreatePluginMigrateByname", function()
+    -- 1. prepend doom- prefix
+    -- 2. add `.nvim` extension
+    -- 3. format `_` to `-` in name.
+    -- 4. use BOILIT to generate plugin template.
+    -- 5. move module segments into plugin files.
+    -- 6. assign the plugin path to variable.
+    -- 7. use REFACTOR to migrate chunks
+    -- 8. add plugin to GHQ
+    -- 9.
+  end },
 }
 
 return create_plugin
