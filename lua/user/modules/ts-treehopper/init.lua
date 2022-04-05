59f5c944a3bcbd24ad446960ec26a3c2487c5b41
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-05 03:53:55 -0400

    user modules

--- lua/user/modules/ts-treehopper/init.lua
+++ lua/user/modules/ts-treehopper/init.lua
@@ -1,11 +1,11 @@
-
 -- https://github.com/mfussenegger/nvim-treehopper
+local ts_hop = {}
 
 local api = vim.api
 local M = {}
-local ns = api.nvim_create_namespace('me.tsnode')
+local ns = api.nvim_create_namespace("me.tsnode")
 
-M.config = {hint_keys = {}}
+M.config = { hint_keys = {} }
 
 local function keys_iter()
   local i = 0
@@ -28,7 +28,9 @@ local function keys_iter()
           end
         end
 
-        if k then return c end
+        if k then
+          return c
+        end
       else
         local c = string.char(i + 65 - 27)
 
@@ -41,23 +43,24 @@ local function keys_iter()
           end
         end
 
-        if k then return c end
+        if k then
+          return c
+        end
       end
     end
   end
 end
-
 
 function M.nodes()
   api.nvim_buf_clear_namespace(0, ns, 0, -1)
   local ts = vim.treesitter
 
   -- copy this into ts monitor
-  local get_query = require('vim.treesitter.query').get_query
+  local get_query = require("vim.treesitter.query").get_query
   local get_parser = require("vim.treesitter").get_parser
-  local query = get_query(get_parser(0)._lang, 'locals')
+  local query = get_query(get_parser(0)._lang, "locals")
   if not query then
-    print('No locals query for language', vim.bo.filetype)
+    print("No locals query for language", vim.bo.filetype)
     return
   end
 
@@ -73,19 +76,19 @@ function M.nodes()
 
   local win_info = vim.fn.getwininfo(api.nvim_get_current_win())[1]
   for i = win_info.topline, win_info.botline do
-    api.nvim_buf_add_highlight(0, ns, 'TSNodeUnmatched', i - 1, 0, -1)
+    api.nvim_buf_add_highlight(0, ns, "TSNodeUnmatched", i - 1, 0, -1)
   end
 
   local function register_node(node)
     local key = iter()
-    local start_row, start_col, end_row, end_col = node:range()
+    local start_row, start_col, end_row, end_col = node:range() -- very interesting that you can get range like this.
     api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
-      virt_text = {{key, 'TSNodeKey'}},
-      virt_text_pos = 'overlay'
+      virt_text = { { key, "TSNodeKey" } },
+      virt_text_pos = "overlay",
     })
     api.nvim_buf_set_extmark(0, ns, end_row, end_col, {
-      virt_text = {{key, 'TSNodeKey'}},
-      virt_text_pos = 'overlay'
+      virt_text = { { key, "TSNodeKey" } },
+      virt_text_pos = "overlay",
     })
     hints[key] = node
   end
@@ -97,7 +100,7 @@ function M.nodes()
     parent = parent:parent()
   end
 
-  vim.cmd('redraw')
+  vim.cmd("redraw")
 
   while true do
     local ok, keynum = pcall(vim.fn.getchar)
@@ -111,11 +114,11 @@ function M.nodes()
       if node then
         local start_row, start_col, end_row, end_col = node:range()
         api.nvim_win_set_cursor(0, { start_row + 1, start_col })
-        vim.cmd('normal! v')
+        vim.cmd("normal! v")
         local max_row = api.nvim_buf_line_count(0)
         if max_row == end_row then
           end_row = end_row - 1
-          end_col = #(api.nvim_buf_get_lines(0, end_row, end_row + 1, true)[1])
+          end_col = #api.nvim_buf_get_lines(0, end_row, end_row + 1, true)[1]
         elseif end_col == 0 then
           -- If the end points to the start of the next line, move it to the
           -- end of the previous line.
@@ -128,18 +131,34 @@ function M.nodes()
         api.nvim_buf_clear_namespace(0, ns, 0, -1)
         break
       else
-        vim.api.nvim_feedkeys(key, '', true)
+        vim.api.nvim_feedkeys(key, "", true)
         api.nvim_buf_clear_namespace(0, ns, 0, -1)
         break
       end
     end -- if number
   end -- while true
-
 end
 
--- binds
---
--- leader n t (nav treesitter)
+ts_hop.binds = {}
 
+-- if require("doom.utils").is_module_enabled("whichkey") then
+--   table.insert(ts_hop.binds, {
+--     "<leader>",
+--     name = "+prefix",
+--     {
+--       {
+--         "n",
+--         name = "+nav",
+--         {
+--           "t",
+--           name = "+treesitter",
+--           {
+--             { "h", , name = "ts hop"}
+--           },
+--         },
+--       },
+--     },
+--   })
+-- end
 
--- return M
+return ts_hop
