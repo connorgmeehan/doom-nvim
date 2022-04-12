51a3ac557adc11dcd52c0d30a37d1a6c3a619688
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-12 02:08:36 -0400

    more user modules

---
+++ lua/user/modules/registers/init.lua
@@ -0,0 +1,3 @@
+-- gennaro-tedesco/nvim-peekup - Dynamically interact with vim registers.
+-- tversteeg/registers.nvim - Non-obtrusive minimal preview of vim registers.
+-- acksld/nvim-neoclip.lua - Clipboard manager Neovim plugin with telescope integration.
