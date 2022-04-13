fdfcacff627e916f0dfb78ca7a58cd20b996fca9
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-13 05:31:29 -0400

    rename user ts modules

--- lua/user/modules/nvim_luadev/init.lua
+++ lua/user/modules/nvim_luadev/init.lua
@@ -12,7 +12,6 @@ luadev.packages = {
   -- ["nvim-luadev"] = { "bfredl/nvim-luadev" }, -- REPL/debug console for Neovim Lua plugins. The :Luadev command will open an scratch window which will show output from executing Lua code.
   -- ["nvim-plugin-template"] = {"ellisonleao/nvim-plugin-template"}, -- Another neovim plugin template, using GitHub's template feature.
   ["nvim-luapad"] = { "rafcamlet/nvim-luapad" }, -- Interactive real time Neovim scratchpad for embedded Lua engine - Type and watch!.
-  ["playground"] = { "nvim-treesitter/playground" },
   -- jbyuki/one-small-step-for-vimkind -- An adapter for the Neovim Lua language. It allows you to debug any Lua code running in a Neovim instance (A Lua plugin that can debug Neovim Lua plugins).
   -- tami5/sqlite.lua -- SQLite/LuaJIT binding for Lua and Neovim.
   -- mini.doc -- Module of echasnovski/mini.nvim for generation of help files from EmmyLua-like annotations. Allows flexible customization of output via hook functions.
---
+++ lua/user/modules/ts_playground/init.lua
@@ -0,0 +1,10 @@
+local ts_playground = {}
+
+ts_playground.settings = {}
+
+ts_playground.packages = {
+  ["playground"] = { "nvim-treesitter/playground" }, -- move to ts module.
+}
+
+return ts_playground
+
