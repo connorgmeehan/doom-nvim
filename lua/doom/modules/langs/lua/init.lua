2a648de30147dfd842e1b1ad9144337d06905f48
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-15 06:44:12 -0400

    fix(langs): set check third party false
    prevents LOVE 3d prompt

--- lua/doom/modules/langs/lua/init.lua
+++ lua/doom/modules/langs/lua/init.lua
@@ -14,6 +14,7 @@ lua.settings = {
       },
       workspace = {
         library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
       },
       telemetry = {
         enable = false,
