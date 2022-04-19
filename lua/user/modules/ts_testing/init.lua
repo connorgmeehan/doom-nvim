f7f125f16e9e3991b44684df00ce05f7dad792a7
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-19 03:34:50 -0400

    user ts testing

--- lua/user/modules/ts_testing/init.lua
+++ lua/user/modules/ts_testing/init.lua
@@ -3,13 +3,51 @@ local ts_utils = require("nvim-treesitter.ts_utils")
 
 -- https://github.com/nvim-treesitter/nvim-treesitter/blob/master/doc/nvim-treesitter.txt
 
+-- 1. nui popup.
+-- 2. display all kinds of siblings/children/parents/block etc.
+
 local ts_testing = {}
 
 -- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
--- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
--- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
--- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+-- UI
+
+local popup_settings = {
+  enter = true,
+  focusable = true,
+  border = {
+    style = "rounded",
+  },
+  position = "50%",
+  size = {
+    width = "80%",
+    height = "60%",
+  },
+  buf_options = {
+    modifiable = true,
+    readonly = false,
+  },
+}
+
+local function make_popup(display_string)
+  local Popup = require("nui.popup")
+  local event = require("nui.utils.autocmd").event
+
+  local popup = Popup(popup_settings)
+
+  -- mount/open the component
+  popup:mount()
+
+  -- unmount component when cursor leaves buffer
+  popup:on(event.BufLeave, function()
+    popup:unmount()
+  end)
+
+  -- set content
+  vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, vim.fn.split(display_string, "\n"))
+end
+
+-- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 -- Lua module: vim.treesitter.language                      *treesitter-language*
 --
@@ -33,94 +71,6 @@ local ts_testing = {}
 --                     {path}    Optional path the parser is located at
 --                     {silent}  Don't throw an error if language not found
 
--- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
--- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-
-
-
-local parsers = require("nvim-treesitter.parsers")
-
---- local myEnum = Enum {
----     'Foo',          -- Takes value 1
----     'Bar',          -- Takes value 2
----     {'Qux', 10},    -- Takes value 10
----     'Baz',          -- Takes value 11
---- }
-
----@class RefactorQuery
-local Query = {}
-Query.__index = Query
-
-Query.query_type = {
-  FunctionArgument = "definition.function_argument",
-  LocalVarName = "definition.local_name",
-  Reference = "reference",
-}
-
-function Query.get_root(bufnr, filetype)
-  local parser = parsers.get_parser(bufnr or 0, filetype)
-  if not parser then
-    error("No treesitter parser found. Install one using :TSInstall <language>")
-  end
-  return parser:parse()[1]:root()
-end
-
-function Query.from_query_name(bufnr, filetype, query_name)
-  local query = vim.treesitter.get_query(filetype, query_name)
-  return Query:new(bufnr, filetype, query)
-end
-
-function Query:new(bufnr, filetype, query)
-  return setmetatable({
-    query = query,
-    bufnr = bufnr,
-    filetype = filetype,
-    root = Query.get_root(bufnr, filetype),
-  }, self)
-end
-
-function Query:pluck_by_capture(scope, captures)
-  if type(captures) ~= "table" then
-    captures = { captures }
-  end
-
-  local out = {}
-  for id, node, _ in self.query:iter_captures(scope, self.bufnr, 0, -1) do
-    local n_capture = self.query.captures[id]
-    for _, capture in pairs(captures) do
-      if n_capture == capture then
-        table.insert(out, node)
-        break
-      end
-    end
-  end
-  return out
-end
-
-function Query.find_occurrences(scope, sexpr, bufnr)
-  local filetype = vim.bo[bufnr].filetype
-
-  if not sexpr:find("@") then
-    sexpr = sexpr .. " @tmp_capture"
-  end
-
-  local ok, sexpr_query = pcall(vim.treesitter.parse_query, filetype, sexpr)
-  if not ok then
-    error(string.format("Invalid query: '%s'\n error: %s", sexpr, sexpr_query))
-  end
-
-  local occurrences = {}
-  for _, n in sexpr_query:iter_captures(scope, bufnr, 0, -1) do
-    table.insert(occurrences, n)
-  end
-
-  return occurrences
-end
-
--- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
--- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
--- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 -- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 -- parse window
@@ -195,40 +145,70 @@ end
 -- >> https://github.com/nvim-treesitter/nvim-treesitter#available-modules
 -- >> :h nvim-treesitter
 
+local function get_node_info_string(node, title)
+  if node == nil then
+    return "none"
+  end
+  local tsn_id = node:id()
+  local tsn_type = node:type()
+  local tsn_symbol = node:symbol()
+  local tsn_range = node:range()
+  local tsn_sexpr = node:sexpr()
+  local tsn_child_count = node:child_count()
+  local tsn_start_row, tsn_start_col, tsn_start_bytecount = node:start()
+  local tsn_end_row, tsn_end_col, tsn_end_bytecount = node:end_()
+
+  local str_start = string.format([[%s, %s,  %s]], tsn_start_row, tsn_start_col, tsn_start_row)
+  local str_end = string.format([[%s, %s, %s]], tsn_end_row, tsn_end_col, tsn_end_row)
+
+  local node_text = ts_helpers.get_node_text(node)
+
+  local ret_str = string.format(
+    [[
+>> %s:
+%s | %s | %s | %s
+%s
+------------------
+		]],
+    title,
+    tsn_symbol,
+    tsn_type,
+    str_start,
+    str_end,
+    node_text
+  )
+  return ret_str
+end
+
+local function get_node_text_preview(node) end
+
 -- Can refactor take the functions below and hoist them to local functions at
 -- the beginning of the file?
 ts_testing.cmds = {
   {
     "TSTestingPrintCursorEnvironment",
     function()
-      local tsnode = ts_helpers.get_node_at_cursor(0) -- current window
-      local tsn_id = tsnode:id()
-      local tsn_type = tsnode:type()
-      local tsn_symbol = tsnode:symbol()
-      local tsn_range = tsnode:range()
-      local tsn_sexpr = tsnode:sexpr()
-      local tsn_child_count = tsnode:child_count()
-      local tsn_start_row, tsn_start_col, tsn_start_bytecount = tsnode:start()
-      local tsn_end_row, tsn_end_col, tsn_end_bytecount = tsnode:end_()
+      local cursor_node = ts_helpers.get_node_at_cursor(0) -- current window
+      local INFO_NODE_CURSOR = get_node_info_string(cursor_node, "cursor node")
 
-      -- get preview text
+      local parent_node = cursor_node:parent()
+      local INFO_NODE_PARENT = get_node_info_string(parent_node, "parent")
 
-      local tsn_parent = tsnode:parent() -- Get the node's immediate parent.
-      local tsn_parent_type = tsn_parent:type()
-      -- local tsn_start_row, tsn_start_col, tsn_start_bytecount = tsnode:start()
-      -- local tsn_end_row, tsn_end_col, tsn_end_bytecount = tsnode:end_()
-
-      -- get parent preview text
-
-      local tsn_sib_next = tsnode:next_sibling() -- Get the node's next sibling.
-      local tsn_sib_prev = tsnode:prev_sibling() -- Get the node's previous sibling.
-      local tsn_sib_next_named = tsnode:next_named_sibling() -- Get the node's next named sibling.
-      local tsn_sib_prev_named = tsnode:prev_named_sibling() -- Get the node's previous named sibling.
+      local siblin_next = cursor_node:next_sibling()
+      local siblin_prev = cursor_node:prev_sibling()
+      local INFO_NODE_SIB_PREV = get_node_info_string(siblin_prev, "siblin_prev")
+      local INFO_NODE_SIB_NEXT = get_node_info_string(siblin_next, "siblin_next")
+      -- local tsn_sib_next_named = cursor_node:next_named_sibling() -- Get the node's next named sibling.
+      -- local tsn_sib_prev_named = cursor_node:prev_named_sibling() -- Get the node's previous named sibling.
 
       -- get prev/next sibling preview texts
 
-      -- tsnode:iter_children() --   Iterates over all the direct children of {tsnode}, regardless of whether they are named or not. Returns the child node plus the eventual field name corresponding to this child node.
-      -- tsnode:field({name})					    --   Returns a table of the nodes corresponding to the {name} field.
+      local child_first = cursor_node:child(1)
+      local child_last = cursor_node:child(cursor_node:child_count())
+      local INFO_NODE_CHILD_FIRST = get_node_info_string(child_first, "child_first")
+      local INFO_NODE_CHILD_LAST = get_node_info_string(child_last, "child_last")
+      -- cursor_node:iter_children() --   Iterates over all the direct children of {cursor_node}, regardless of whether they are named or not. Returns the child node plus the eventual field name corresponding to this child node.
+      -- cursor_node:field({name})					    --   Returns a table of the nodes corresponding to the {name} field.
       -- tsnode:child({index})					    -- Get the node's child at the given {index}, where zero represents the first child.
       -- tsnode:named_child_count() --   Get the node's number of named children.
       -- tsnode:named_child({index})				-- Get the node's named child at the given {index}, where zero represents the first named child.
@@ -237,52 +217,21 @@ ts_testing.cmds = {
 
       local s = string.format(
         [[
-
-        :: CURSOR NODE ::
-
-        ID      ->  %s
-        TYPE    ->  %s
-        SYMBOL  ->  %s
-        SEXPR   ->  %s
-        START   ->  row: %s, col: %s, bytecount: %s
-        END     ->  row: %s, col: %s, bytecount: %s
-
-        :: PARENT ::
-
-        type    -> %s
-
-        :: SIBLINGS ::
-
+%s
+%s
+%s
+%s
+%s
+%s
 ]],
-        tsn_id,
-        tsn_type,
-        tsn_symbol,
-        tsn_sexpr,
-
-        tsn_start_row,
-        tsn_start_col,
-        tsn_start_bytecount,
-        tsn_end_row,
-        tsn_end_col,
-        tsn_end_bytecount,
-
-        tsn_parent_type
-        -- tsnode:range(),
-        -- tsnode:sexpr(),
-        -- tsnode:id(),
-        -- tsnode:parent(), -- Get the node's immediate parent.
-        -- tsnode:next_sibling(), -- Get the node's next sibling.
-        -- tsnode:prev_sibling(), -- Get the node's previous sibling.
-        -- tsnode:next_named_sibling(), -- Get the node's next named sibling.
-        -- tsnode:prev_named_sibling(), -- Get the node's previous named sibling.
-        -- tsnode:iter_children(), --   Iterates over all the direct children of {tsnode}, regardless of whether they are named or not. Returns the child node plus the eventual field name corresponding to this child node.
-        -- -- tsnode:field({name}),					    --   Returns a table of the nodes corresponding to the {name} field.
-        -- tsnode:child_count(), --   Get the node's number of children.
-        -- -- tsnode:child({index}),					    -- Get the node's child at the given {index}, where zero represents the first child.
-        -- tsnode:named_child_count() --   Get the node's number of named children.
-        -- -- tsnode:named_child({index}),				-- Get the node's named child at the given {index}, where zero represents the first named child.
+        INFO_NODE_CURSOR,
+        INFO_NODE_PARENT,
+        INFO_NODE_SIB_PREV,
+        INFO_NODE_SIB_NEXT,
+        INFO_NODE_CHILD_FIRST,
+        INFO_NODE_CHILD_LAST
       )
-      print(s)
+      make_popup(s)
     end,
   },
   {
