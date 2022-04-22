a69c5d103e43758819a0380e9dc4f08df5d00605
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-22 06:50:19 -0400

    user ts modules

--- lua/user/modules/ts_query_monitor/init.lua
+++ lua/user/modules/ts_query_monitor/init.lua
@@ -14,24 +14,75 @@ local ts_query_monitor = {}
 
 local AUTOCMD_PREFIX = "UserTSQueryMonitor_"
 
-local query_lang = "java"
+-- local query_lang = "lua"
 
 local query_str = [[
-(method_declaration
-    (modifiers
-        (marker_annotation
-            name: (identifier) @annotation (#eq? @annotation "Test")))
-    name: (identifier) @method (#offset! @method))
+; Scopes
+
+[
+  (chunk)
+  (do_statement)
+  (while_statement)
+  (repeat_statement)
+  (if_statement)
+  (for_statement)
+  (function_declaration)
+  (function_definition)
+] @scope
+
+; Definitions
+
+(assignment_statement
+  (variable_list
+    (identifier) @definition.var))
+
+(assignment_statement
+  (variable_list
+    (dot_index_expression . (_) @definition.associated (identifier) @definition.var)))
+
+; (function_declaration
+;   name: (identifier) @definition.function)
+;   (#set! definition.function.scope "parent")
+;
+; (function_declaration
+;   name: (dot_index_expression
+;     . (_) @definition.associated (identifier) @definition.function))
+;   (#set! definition.method.scope "parent")
+;
+; (function_declaration
+;   name: (method_index_expression
+;     . (_) @definition.associated (identifier) @definition.method))
+;   (#set! definition.method.scope "parent")
+;
+; (for_generic_clause
+;   (variable_list
+;     (identifier) @definition.var))
+;
+; (for_numeric_clause
+;   name: (identifier) @definition.var)
+;
+; (parameters (identifier) @definition.parameter)
+
+; References
+
+[
+  (identifier)
+] @reference
+
 ]]
 
 local references = {}
 
 ts_query_monitor.settings = {
+  buf_name = "TS_MONITOR",
   popup = {
     enter = false,
     focusable = false,
     border = {
       style = "rounded",
+      text = {
+        top = "TS MONITOR",
+      },
     },
     position = {
       row = "5%",
@@ -48,11 +99,15 @@ ts_query_monitor.settings = {
   },
 }
 
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 local function i(value)
   print(vim.inspect(value))
 end
 
-local function get_query(the_lang, query_name)
+-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+local function get_query(query_name)
   local language_tree = vim.treesitter.get_parser(0)
   local syntax_tree = language_tree:parse()
   local root = syntax_tree[1]:root()
@@ -60,23 +115,26 @@ local function get_query(the_lang, query_name)
 
   local matches = {}
 
+  -- opts.ft
+  -- opts.query
+  local the_lang = query_lang or vim.bo.filetype
+
   local ok, result = pcall(vim.treesitter.parse_query, the_lang, query_name)
 
   if ok then
-    for _, captures, metadata in result:iter_matches(root, references.target_bufnr) do
-      i(q.get_node_text(captures[2], references.target_bufnr))
-      i(metadata)
-      table.insert(matches, q.get_node_text(captures[2], references.target_bufnr))
-    end
+    -- for _, captures, metadata in result:iter_matches(root, references.target_bufnr) do
+    --   i(q.get_node_text(captures[2], references.target_bufnr))
+    --   -- i(metadata)
+    --   table.insert(matches, q.get_node_text(captures[2], references.target_bufnr))
+    -- end
     return table.concat(matches, sep)
   else
     return result
   end
-
 end
 
 local function popup_set_content()
-  local str_display = get_query(query_lang, query_str)
+  local str_display = get_query(query_str)
   vim.api.nvim_buf_set_lines(references.popup.bufnr, 0, 1, false, vim.fn.split(str_display, "\n"))
 end
 
@@ -84,34 +142,32 @@ local function refresh_content()
   popup_set_content()
 end
 
-local function detach_autocmds()
-  cmd(string.format("autocmd! %s%d BufLeave", AUTOCMD_PREFIX, references.target_bufnr))
-end
-
 local function reset_monitor()
-  detach_autocmds()
-  references.popup:unmount()
+  if references.popup ~= nil then
+    references.popup:unmount()
+  end
   references.target_bufnr = nil
   references.popup = nil
 end
 
-local function on_buf_leave()
+local function detach_and_reset(bufnr)
+  cmd(string.format("autocmd! %s%d WinLeave,BufLeave", AUTOCMD_PREFIX, bufnr))
   reset_monitor()
 end
 
-local function attach_autocmds()
-  utils.make_augroup(string.format("%s%d", AUTOCMD_PREFIX, references.target_bufnr), {
-    -- update monitor content
+local function attach_autocmds(bufnr)
+  utils.make_augroup(string.format("%s%d", AUTOCMD_PREFIX, bufnr), {
     {
       "InsertChange,InsertLeave",
-      string.format("<buffer=%d>", references.target_bufnr),
+      string.format("<buffer=%d>", bufnr),
       refresh_content,
     },
-    -- kill monitor on target leave
     {
-      "BufLeave",
-      string.format("<buffer=%d>", references.target_bufnr),
-      on_buf_leave,
+      "WinLeave,BufLeave",
+      string.format("<buffer=%d>", bufnr),
+      function()
+        detach_and_reset(bufnr)
+      end,
     },
   })
 end
@@ -125,8 +181,11 @@ local function spawn_monitor()
   popup:mount()
   references.popup = popup
 
+  -- rename monitor buffer
+  vim.api.nvim_buf_set_name(popup.bufnr, ts_query_monitor.settings.buf_name)
+
   popup_set_content()
-  attach_autocmds(target_bufnr)
+  attach_autocmds(references.target_bufnr)
 end
 
 local function toggle_query_monitor()
---
+++ lua/user/modules/ts_textobjects/init.lua
@@ -0,0 +1,12 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+--
+--
+-- create a bind for each entry according to the readme.
+
+local textobjects = {}
+
+textobjects.packages = {
+  ["nvim-treesitter-textobjects"] = { "nvim-treesitter/nvim-treesitter-textobjects" },
+}
+
+return textobjects
