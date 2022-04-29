6f0a7476a33fa02a6433352ae36b25c88c984eb6
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-29 04:25:32 -0400

    query: binds query v1 works

--- lua/user/modules/create_binds/init.lua
+++ lua/user/modules/create_binds/init.lua
@@ -1,3 +1,11 @@
+local tsq = require("vim.treesitter.query")
+local ts_utils = require("nvim-treesitter.ts_utils")
+local locals = require("nvim-treesitter.locals")
+local api = vim.api
+local cmd = api.nvim_command
+local doom_queries = require("user.utils.doom_queries")
+local user_ts_utils = require("user.utils.ts")
+
 local create_binds = {}
 
 -- TODO:
@@ -6,8 +14,28 @@ local create_binds = {}
 --    - cmd: DoomBindsLeaderAddBindToLevelUnderCursor
 --    - cmd: DoomBindsAddRegularBind
 --    		telescope picker -> select module -> enter module.binds table and insert a regular bind
+--
+--    this is going to be very interesting to do a telescope picker for.
+--
+--    1. get all modules
+--    2. make picker
+--    3. enter new binds position in module.
+--    4. enter leader position
+--    5. use with snippets.
+--
+--
+--    BINDS NAVIGATION
+--
+--    make commands for jumping between command tables quickly.
+--
+--    next/prev bind
+--    next/prev leader bind
+--    add child
+--    jump next child.
+--
+--    insert leader branch
 
--- create_binds.settings = {}
+create_binds.settings = {}
 
 -- create_binds.packages = {
 -- [""] = {},
@@ -16,9 +44,72 @@ local create_binds = {}
 -- -- [""] = {},
 -- }
 
--- create_binds.cmds = {}
+local function create_bind_for_select_module()
+  print("create binds...")
+
+  -- TODO: need to read query into string first
+  local bufnr, root, parsed_query = user_ts_utils.get_query(
+    user_ts_utils.get_query_file("lua", "doom_module_binds")
+  )
+
+  -- local package_string_nodes = user_ts_utils.get_captures(root, bufnr, parsed_query, "package_string")
+
+  user_ts_utils.log_captures(root, bufnr, parsed_query)
+
+  -- local picker_config = {
+  --   bufnr = bufnr,
+  --   entries = {},
+  --   entries_mapped = {},
+  --   callback = fork_package,
+  -- }
+  -- for k, v in pairs(package_string_nodes) do
+  --   local nt = tsq.get_node_text(v, bufnr)
+  --   table.insert(picker_config.entries, nt)
+  --   picker_config.entries_mapped[nt] = v
+  -- end
+  -- fork_plugins_picker_cur_buf(picker_config)
+end
+
+create_binds.cmds = {
+  {
+    "DoomCreateBindForSelectModule",
+    function()
+      create_bind_for_select_module()
+    end,
+  },
+}
+
 -- create_binds.autocmds = {}
--- create_binds.binds = {}
+
+create_binds.binds = {
+  { "gXX", "jj", name = "dummytest" },
+  {
+    "<leader>",
+    name = "+prefix",
+    {
+      {
+        "D",
+        name = "+doom",
+        {
+          {
+            "B",
+            [[ :DoomCreateBindForSelectModule<cr> ]],
+            name = "create bind for mod",
+            options = { silent = false },
+          },
+          { "X", "jj", name = "doom XX" },
+        },
+      },
+    },
+    {
+      {
+        "X",
+        name = "+XXX",
+        {},
+      },
+    },
+  },
+}
 
 -- if require("doom.utils").is_module_enabled("whichkey") then
 --   table.insert(create_binds.binds, {
@@ -26,10 +117,10 @@ local create_binds = {}
 --     name = "+prefix",
 --     {
 --       {
---         "YYY",
---         name = "+ZZZ",
+--         "D",
+--         name = "+doom",
 --         {
---         -- first level
+--           { "B", [[ :DoomCreateBindForSelectModule<cr> ]], name = "create bind for mod" },
 --         },
 --       },
 --     },
@@ -37,4 +128,3 @@ local create_binds = {}
 -- end
 
 return create_binds
-
