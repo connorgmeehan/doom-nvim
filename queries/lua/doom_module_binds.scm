99211dc5c8f82f1e0874af7a14239b3cfb5f1dca
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-29 03:02:48 -0400

    queries: module binds

--- queries/lua/doom_module_binds.scm
+++ queries/lua/doom_module_binds.scm
@@ -1,29 +1,55 @@
 (assignment_statement
+
   (variable_list
-    name: (dot_index_expression field: (identifier))) ; #eq "binds"
+    name: (dot_index_expression field: (identifier) @binds-table)) ; #eq "binds"
+    (#eq? @binds-table "binds")
+
   (expression_list
     value: (table_constructor
-      (field)*; each regular bind comes first
-      (field ; this is the leader field. how can we caputure these?
-	value: (table_constructor
-	  (field value: (string)) (field name: (identifier) value: (string))
-          (field value: (table_constructor
-	    [(field)(field
-	     (field value: (string)) (field name: (identifier) value: (string))
-	     (field value: (table_constructor
-	          [(field)(field
-	           (field value: (string)) (field name: (identifier) value: (string))
-	           (field value: (table_constructor
-	                [(field)(field
-	                 (field value: (string)) (field name: (identifier) value: (string))
-	                 (field value: (table_constructor
-			      [(field)(field
 
-	            	  	)
-	            	      ]*)))
-	                ]*)))
-	          ]*)))
-           ]*))))
-  ))
-)
+      ; regular bind
+      (field)* @binds-regular ; zero or more regular binds
 
+      ; leader table
+      (field value: (table_constructor
+      (field value: (string) @leader-name)
+      (field name: (identifier) value: (string) @leader-prefix)
+      (#eq? @leader-name "<leader>")
+      (#eq? @leader-prefix "+prefix")
+      (field value: (table_constructor [
+
+	;;;;;;;;;;;;;;;;
+
+	(field)* @lvl1bind.table
+	(field value:
+	(table_constructor
+	(field value: (string) @lvl1branch.char)
+	(field name: (identifier) value: (string) @lvl1branch.name)
+	(field value: (table_constructor [
+
+	  (field)* @lvl2bind.table
+	  (field value:
+	  (table_constructor
+	  (field value: (string) @lvl2branch.char)
+	  (field name: (identifier) value: (string) @lvl2branch.name)
+	  (field value: (table_constructor [
+
+	    (field)* @lvl3bind.table
+	    (field value:
+   	    (table_constructor
+	    (field value: (string) @lvl3branch.char)
+	    (field name: (identifier) value: (string) @lvl3branch.name)
+	    (field value: (table_constructor
+
+	    )))) @lvl3branch.table
+
+	  ]*)))) @lvl2branch.table
+
+	]*)))) @lvl1branch.table
+
+	;;;;;;;;;;;;;;;;
+
+      ]*))))? @leader-table ; ? -> optional table
+    ) ; table_constructor
+  ) ; expression_list
+) ; assignment_statement
