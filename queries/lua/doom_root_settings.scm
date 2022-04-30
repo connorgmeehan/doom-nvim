ebea2339219c43822e907e5f5e1ea05ee97f9ef7
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-04-30 08:55:48 -0400

    queries: user settings query

---
+++ queries/lua/doom_root_settings.scm
@@ -0,0 +1,7 @@
+(return_statement
+  (expression_list
+    (table_constructor
+      (field
+        name: (identifier)
+        value: (table_constructor
+          (field) @settings.field )) @settings.table )))
