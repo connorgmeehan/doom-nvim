0ad51c202030aaab45a96bfacfc2d815cf4a8d43
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2022-05-01 04:08:12 -0400

    module_manager: wip

--- lua/user/modules/module_manager/init.lua
+++ lua/user/modules/module_manager/init.lua
@@ -3,7 +3,47 @@ local user_utils_ui = require("user.utils.ui")
 local user_utils_path = require("user.utils.path")
 local user_utils_modules = require("user.utils.modules")
 
-local module_rename = {}
+local M = {}
+
+M.settings = {
+  popup = {
+    relative = "cursor",
+    position = {
+      row = 1,
+      col = 0,
+    },
+    size = 20,
+    border = {
+      style = "rounded",
+      text = {
+        top = "[Input]",
+        top_align = "left",
+      },
+    },
+    win_options = {
+      winhighlight = "Normal:Normal",
+    },
+  },
+  menu = {
+    position = "20%",
+    size = {
+      width = 20,
+      height = 2,
+    },
+    relative = "editor",
+    border = {
+      style = "single",
+      text = {
+        top = "Choose Something",
+        top_align = "center",
+      },
+    },
+    win_options = {
+      winblend = 10,
+      winhighlight = "Normal:Normal",
+    },
+  },
+}
 
 -- TODO:
 --
@@ -17,19 +57,88 @@ local module_rename = {}
 -- 		b. check name aleady exists? > open nui window again?
 --
 --    	c. update module dirname
-local function promt_for_new_name() end
+
+local function m_name_exists(new_name)
+  -- return bool
+end
+
+local function nui_input(title, callback)
+  local Input = require("nui.input")
+  local event = require("nui.utils.autocmd").event
+  local input = Input(M.settings.popup, {
+    prompt = title .. "> ",
+    default_value = "",
+    on_close = function()
+      print("Input closed!")
+    end,
+    on_submit = function(value)
+      callback(value)
+    end,
+    on_change = function(value)
+      print("Value changed: ", value)
+    end,
+  })
+  input:mount()
+  input:on(event.BufLeave, function()
+    input:unmount()
+  end)
+end
+
+local function nui_menu(title, callback)
+  local Menu = require("nui.menu")
+  local event = require("nui.utils.autocmd").event
+
+  local menu = Menu(M.settings.menu, {
+    lines = {
+      Menu.item("Item 1"),
+      Menu.item("Item 2"),
+      Menu.separator("Menu Group", {
+        char = "-",
+        text_align = "right",
+      }),
+      Menu.item("Item 3"),
+    },
+    max_width = 20,
+    keymap = {
+      focus_next = { "j", "<Down>", "<Tab>" },
+      focus_prev = { "k", "<Up>", "<S-Tab>" },
+      close = { "<Esc>", "<C-c>" },
+      submit = { "<CR>", "<Space>" },
+    },
+    on_close = function()
+      print("CLOSED")
+    end,
+    on_submit = function(item)
+      callback(item)
+    end,
+  })
+
+  -- mount the component
+  menu:mount()
+
+  -- close menu when cursor leaves buffer
+  menu:on(event.BufLeave, menu.menu_props.on_close, { once = true })
+end
 
 --
 -- ACTION FUNCTIONS -> params: (buf, config, module, telescope_input)
 --
 
 local function m_rename(b, c, m, i)
-  print("M RENAME: ", i)
+  -- print("M RENAME: ", i)
+  nui_input("rename", function(value)
+    print("value:", value)
+  end)
   -- 1. transform `modules.lua` with architext
   -- 2. change module dir name.
 end
+
 local function m_create(b, c, m, i)
   print("M CREATE: ", i)
+  -- 1. create dir
+  -- 2. init new template file
+  -- 3. transform `modules.lua`
+
   -- local path_user_modules = string.format("%s/lua/user/modules", system.doom_root)
   -- local new_module_path = string.format("%s%s%s", path_user_modules, system.sep, new_mname)
   -- local new_module_init_file = string.format("%s%sinit.lua", new_module_path, system.sep)
@@ -46,19 +155,29 @@ local function m_create(b, c, m, i)
   -- -- set file to cur bef
   -- -- vim.cmd(string.format(":%sb", newbuf))
 end
+
 local function m_edit(buf, config, m, i)
   print("M EDIT: ", i)
+  -- 1. open module `init.lua`
   -- vim.cmd(string.format(":e %s/lua/user/modules/%s/init.lua", system.doom_root, mname))
 end
-local function m_delete(buf, config)
+
+local function m_delete(buf, c, m, i)
   print("M DELETE: ", i)
-  -- 1. nui menu -> are you sure
+  nui_menu("delete", function(value)
+    print("value:", value)
+  end)
+  -- 1. nui menu -> are you sure you want to delete module `XXX`
   -- 2. if yes ->
   -- 	a. transform `modules.lua`
-  -- 	b. remove dir.
+  -- 	b. delete dir.
 end
+
 local function m_toggle(buf, config)
   print("M TOGGLE: ", i)
+  -- 1. select module
+  -- 2. transform `modules.lua`
+
   -- if opts.enabled then
   --   user_ts_utils.ts_nodes_prepend_text(nodes_by_section, buf, "-- ")
   -- else
@@ -67,10 +186,19 @@ local function m_toggle(buf, config)
   --   -- print("root modules rm comment > todo..")
   -- end
 end
+
 local function m_move(buf, config)
   -- move module into into (features/langs)
+  -- 1. nui menu select ( other sections than self)
+  -- 2. move module dir
+  -- 3. transform `modules.lua`
 end
-local function m_merge(buf, config) end
+
+local function m_merge(buf, config)
+  -- 1. new prompt for B
+  -- 2. select which module to pull into A
+  -- 3. do...
+end
 -- action_id = "BIND"
 -- action_id = "LEADER"
 -- action_id = "CMD"
@@ -90,6 +218,9 @@ end
 
 local function picker_action_router(buf, config, action, use_line)
   local fuzzy, line = picker_get_state(buf)
+
+  require("telescope.actions").close(buf)
+
   local m_sel = config.entries_mapped[fuzzy.value]
   if action == "CREATE" then
     m_create(buf, config, m_sel, line)
@@ -98,12 +229,12 @@ local function picker_action_router(buf, config, action, use_line)
   elseif action == "EDIT" then
     m_edit(buf, config, m_sel, fuzzy.value)
   elseif action == "DELETE" then
+    m_delete(buf, config, m_sel, fuzzy.value)
   elseif action == "TOGGLE" then
     -- elseif action == "MOVE" then
     -- elseif action == "MERGE" then
     -- elseif action == "BINDS" then
   end
-  require("telescope.actions").close(buf)
 end
 
 --
@@ -153,16 +284,14 @@ local function prepare_data_for_picker()
   picker_rename(picker_config)
 end
 
--- module_rename.settings = {}
-
--- module_rename.packages = {
+-- M.packages = {
 -- [""] = {},
 -- -- [""] = {},
 -- -- [""] = {},
 -- -- [""] = {},
 -- }
 
-module_rename.cmds = {
+M.cmds = {
   {
     "DoomModulesRenameSelect",
     function()
@@ -171,11 +300,11 @@ module_rename.cmds = {
   },
 }
 
--- module_rename.autocmds = {}
-module_rename.binds = {}
+-- M.autocmds = {}
+M.binds = {}
 
 if require("doom.utils").is_module_enabled("whichkey") then
-  table.insert(module_rename.binds, {
+  table.insert(M.binds, {
     "<leader>",
     name = "+prefix",
     {
@@ -187,7 +316,7 @@ if require("doom.utils").is_module_enabled("whichkey") then
             {
               "M",
               name = "+modules",
-              { "r", [[ :DoomModulesRenameSelect<cr> ]], name = "rename" },
+              { "M", [[ :DoomModulesRenameSelect<cr> ]], name = "rename" },
             },
           },
         },
@@ -196,4 +325,4 @@ if require("doom.utils").is_module_enabled("whichkey") then
   })
 end
 
-return module_rename
+return M
